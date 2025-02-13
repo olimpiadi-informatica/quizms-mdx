7f6311f9ac4468ad849011dd6a8838c4a46a42b7
Francesco Lugli <francilu26@gmail.com>
2025-02-13 01:08:52 +0100

    fix blockly subproblems schema

--- src/components/blockly.tsx
+++ src/components/blockly.tsx
@@ -1,22 +1,29 @@
 import { BlocklyClient, type BlocklyProps } from "./blockly/workspace";
-import { JsonArray, JsonField, JsonObject } from "./json";
+import { JsonArray, JsonField, JsonObject, Token } from "./json";
 
 export function Blockly(props: BlocklyProps) {
   return (
     <>
-      <JsonField field="type" value="text" />
-      <JsonField field="options">
-        <JsonArray>
-          <JsonObject>
-            <JsonField field="value" value="✅" />
-            <JsonField field="correct" value={true} />
-          </JsonObject>
-          <JsonObject>
-            <JsonField field="value" value="❌" />
-            <JsonField field="correct" value={false} />
-          </JsonObject>
-        </JsonArray>
-      </JsonField>
+      {props.testcases.map((_, i) => (
+        <>
+          {i !== 0 && <Token key={`open${i}`} value="{" />}
+          <JsonField key={`id${i}`} field="id" value={i + 1} />
+          <JsonField key={`type${i}`} field="type" value="text" />
+          <JsonField key={`options${i}`} field="options">
+            <JsonArray>
+              <JsonObject>
+                <JsonField field="value" value="✅" />
+                <JsonField field="correct" value={true} />
+              </JsonObject>
+              <JsonObject>
+                <JsonField field="value" value="❌" />
+                <JsonField field="correct" value={false} />
+              </JsonObject>
+            </JsonArray>
+          </JsonField>
+          i !== props.testcases.length - 1 && <Token key={`close${i}`} value="}," />
+        </>
+      ))}
       <BlocklyClient {...props} />
     </>
   );
