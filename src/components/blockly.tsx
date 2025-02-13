f45adc3850aab54cfde0a93f435af394e82d2da1
Francesco Lugli <francilu26@gmail.com>
2025-02-13 01:37:20 +0100

    quickfix linter sad

--- src/components/blockly.tsx
+++ src/components/blockly.tsx
@@ -6,10 +6,10 @@ export function Blockly(props: BlocklyProps) {
     <>
       {props.testcases.map((_, i) => (
         <>
-          {i !== 0 && <Token key={`open${i}`} value="{" />}
-          <JsonField key={`id${i}`} field="id" value={i + 1} />
-          <JsonField key={`type${i}`} field="type" value="text" />
-          <JsonField key={`options${i}`} field="options">
+          {i !== 0 && <Token value="{" />}
+          <JsonField field="id" value={i + 1} />
+          <JsonField field="type" value="text" />
+          <JsonField field="options">
             <JsonArray>
               <JsonObject>
                 <JsonField field="value" value="✅" />
@@ -21,7 +21,7 @@ export function Blockly(props: BlocklyProps) {
               </JsonObject>
             </JsonArray>
           </JsonField>
-          i !== props.testcases.length - 1 && <Token key={`close${i}`} value="}," />
+          i !== props.testcases.length - 1 && <Token value="}," />
         </>
       ))}
       <BlocklyClient {...props} />
